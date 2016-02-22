2f46c29da106f87b514c670e7c20f014a6baeb16
Arman <thisiisarman@gmail.com>
2016-02-22 21:33:52 +0330

    TelegramLuaBot V1

---
+++ config.lua
@@ -0,0 +1,31 @@
+return {
+	bot_api_key = '',
+	time_offset = 0,
+	cli_port = 4567,
+	admin = 102490691,
+		realm = 0000000,
+			realm_name = 'YOUR REALM',
+	admin_name = 'YOU ,
+
+	errors = {
+		connection = 'Connection error.',
+		results = 'No results found.',
+		argument = 'Invalid argument.',
+		syntax = 'Invalid syntax.',
+		not_admin = 'ERR! NOT ADMIN.'
+	},
+	moderation = {
+		admins = {
+			['000000000'] = 'You'
+		},
+				realms = {
+			['00000000'] = 'MyRealm! '
+		},
+		admin_group = 0000000,
+		realm_name = 'MyRealm'
+	},
+	plugins = {
+		'Help.lua',
+		'Pv.lua',
+	}
+}
