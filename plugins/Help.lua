9dd93c4cb555d76216f4d80e29ff9ef9741ddeca
Arman <thisiisarman@gmail.com>
2016-02-22 21:22:20 +0330

    TelegramLuaBot V1

---
+++ plugins/Help.lua
@@ -0,0 +1,28 @@
+local triggers = {
+	'^/help[@'..bot.username..']*',
+	'^/start[@'..bot.username..']*'
+}
+
+local action = function(msg)
+    if string.match(msg.text, '^/help') then
+        mystat('help')
+    else
+        mystat('usrs')
+    end
+    local text = ''
+    if msg.chat.type == 'group' or msg.chat.type == 'supergroup' then
+        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO ARMAN PV!
+
+        sendReply(msg, 'I\'ve sent you the requested information in private.\nIf you have never used me before, please start a conversation with me and ask for help here again.')
+    end
+    
+    if msg.chat.type == 'private' then
+        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
+    end
+    sendMessage(msg.from.id, text, true, false, true)
+end
+
+return {
+	action = action,
+	triggers = triggers
+}
