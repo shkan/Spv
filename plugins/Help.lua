19057068cf45bc921c0b6a5e59ebf073e0f1b9b0
shkan <ashkankashani80@gmail.com>
2016-03-13 13:29:12 +0330

    Update Help.lua

--- plugins/Help.lua
+++ plugins/Help.lua
@@ -7,13 +7,13 @@ local action = function(msg)
     if string.match(msg.text, '^/help') then
     local text = ''
     if msg.chat.type == 'group' or msg.chat.type == 'supergroup' then
-        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO ARMAN PV!
+        text = text..'*WELCOME TO S.H.O.S PV!*\n[Developer](telegram.me/shayan123hacker)\n_Send Pm With_\n/`/a YourPm!`\n'  --You Can Replace WELCOME TO ARMAN PV!
 
         sendReply(msg, 'I\'ve send you the requested information in private.\nIf you have never used me before, please start a conversation with me and ask for help here again.')
     end
     
     if msg.chat.type == 'private' then
-        text = text..'*WELCOME TO ARMAN PV!*\n[GitHub](github.com/thisisarman)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
+        text = text..'*WELCOME TO S.H.O.S PV!*\n[Developer](telegram.me/shayan123hacker)\n_Send Pm With_\n`/a YourPm!`\n' --You Can Replace WELCOME TO ARMAN PV!
     end
     sendMessage(msg.from.id, text, true, false, true)
 end
